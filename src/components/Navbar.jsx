4cd362bbdc74b03dfc951e97034e6633ed377e04
Ajay Kumar <ajaykumar@spicegems.com>
2025-02-21 16:03:30 +0530

    update lenght issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,7 @@ const Navbar = () => {
         <div className="logo">
             <span className='font-bold text-xl mx-8'>ToDo List</span>
         </div>
-      <ul className="flex gap-8 mx-9">
-        <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
-        <li className='cursor-pointer hover:font-bold transition-all'>Your Tasks</li>
-      </ul>
+    
     </nav>
   )
 }
